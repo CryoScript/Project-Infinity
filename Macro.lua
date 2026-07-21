db1df28c011cccffdad4b5bdebe084ed1e9ed178
CryoScript <140853779+CryoScript@users.noreply.github.com>
2026-07-21 04:20:03 -0500

    Update version number to 16.2.1

--- Macro.lua
+++ Macro.lua
@@ -1,4 +1,4 @@
 --[[
-     Version: 16.0.0
+     Version: 16.2.1
 ]]
 loadstring(game:HttpGet("https://obscuravm.com/protected/cd6456ef99bc445e1e252e01/2aad914aee80c8eda90746b7d05a28b74351528220b73094/download"))()
